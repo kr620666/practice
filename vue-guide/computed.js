134e53c78863d8a8d2a188e83497fcc79782201e
kr620666 <kr620666@amway.com>
2021-07-06 16:05:12 +0900

    커밋 안됐던 파일

--- vue-guide/computed.js
+++ vue-guide/computed.js
@@ -11,3 +11,83 @@ var vm = new Vue({
     },
   },
 });
+
+console.log(vm.reversedMessage); // => 요세하녕항
+vm.message = "Goodbye";
+console.log(vm.reversedMessage); // => 'eybdooG'
+
+var vm1 = new Vue({
+  el: "#example1",
+  data: {
+    message: "호두야잘자",
+  },
+  methods: {
+    reversedMessage: function () {
+      return this.message.split("").reverse().join("");
+    },
+  },
+});
+
+/**
+ * #Computed 속성의 캐싱 vs 메소드
+ *
+ * 최종 결과에 대해 두가지 접근방식은 서로 동일.
+ * 차이점
+ * computed 속성은 종속 대상을 따라 저장(캐싱)된다
+ * 해당 속성이 종속 된 대상이 변경 될떄만 함수를 실행 함.
+ * -> message가 변경되지 않는 한,
+ * computed 속성인 reversedMessage를 여러번 요청해도 계산을 다시 하지 않고
+ * 계산 되어 있던 결과를 즉시 반환.
+ * 또, Date.now() 처럼 아무 곳에도 의존하지 않는 computed 속성의 경우 절대로 업데이트 되지 않음.
+ *
+ * 메소드 호출 시 렌더링을 다시 할 때 마다 항상 함수를 실행 함.
+ *
+ * 캐싱이 필요 한 이유
+ * 계산에 시간이 많이걸리는 computed 속성인 A
+ * 캐싱을 하지 않으면 A의 getter 함수를 꼭 필요한 것보다 더 많이 실행 하게 됨.
+ * 캐싱을 원하지 않는 경우 메소드 사용.
+ *
+ *
+ * #Computed 속성 vs watch 속성
+ *
+ * Vue는 Vue 인스턴스의 데이터 변경을 관찰하고 이에 반응하는 일반적인 watch 속성 제공.
+ * 다른 데이터 기반으로 변경할 필요가 있는 데이터가 있는 경우 사용.
+ * 명령적인 watch 콜백보다 computed 속성을 사용하는 것이 좋음.
+ *
+ * watch 속성은 감시할 데이터를 지정하고 그 데이터가 바뀌면 실행하라는 '명령형 프로그래밍' 방식
+ * computed 속성은 계산해야 하는 목표 데이터를 정의하는 방식 '선언형 프로그래밍' 방식
+ *
+ */
+var vm2 = new Vue({
+  el: "#demo",
+  data: {
+    firstName: "Foo",
+    lastName: "Bar",
+    fullName: "Foo Bar",
+  },
+  watch: {
+    firstName: function (val) {
+      this.fullName = val + " " + this.lastName;
+    },
+    lastName: function (val) {
+      this.fullName = this.firstName + " " + val;
+    },
+  },
+});
+/* 위의 코드는 명령형. 코드 반복함. 
+ 아래 코드는 선언형.
+ 일반적으로 선언형 프로그래밍이 명령형 프로그래밍보다 코드 반복이 적은 등 우수하다고 평가.
+*/
+
+var vm3 = new Vue({
+  el: "#demo2",
+  data: {
+    firstName: "Foo",
+    lastName: "Bar",
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    },
+  },
+});
--- vue-guide/list_rendering.js
+++ vue-guide/list_rendering.js
@@ -24,6 +24,41 @@ new Vue({
   },
 });
 
+/* 
+  배열 변경 감지
+
+  # 변이 메소드
+  Vue는 감시중인 배열의 변이 메소드를 래핑하여 뷰 갱신을 트리거
+  래핑된 메소드
+  - push()
+  - pop()
+  - shift()
+  - unshift()
+  - splice() 
+  - sort()
+  - reverse()
+
+  콘솔에 이전 예제의 items 배열로 변이 메소드를 호출하여 재생 가능
+  example1.items.push({ message: 'Baz' })
+
+  # 배열 대체
+
+  변이 메소드는 호출된 원본 배열을 변형함.
+  변형하지 않는 방법 filter(), concat(), slice()
+  원본 배열을 변형하지 않고 항상 새 배열을 반환함.
+  변형이 없는 방법으로 작업할 때 이전 배열을 새 배열로 바꿀 수 있음.
+
+  배열을 겹치는 객체가 포함 된 다른 배열로 대체하여 효율적임.
+
+  # 주의 사항
+
+  JavaScript의 제한으로 배열에 대해 변경사항을 감지할 수 없는 것.
+  1. 인덱스로 배열에 있는 항목을 직접 설정하는 경우
+      ex) vm.items[indexOfItem] = newValue
+  2. 배열 길이를 수정하는 경우
+      ex) vm.items.length = newLength
+  */
+
 example1.items = example1.items.filter(function (item) {
   return item.message.match(/Foo/);
 });
@@ -33,5 +68,99 @@ var vm = new Vue({
     items: ["a", "b", "c"],
   },
 });
+
 vm.items[1] = "x"; // reactive하지 않음.
 vm.items.length = 2; // reactive하지 않음.
+
+// Vue.set
+Vue.set(vm.items, indexOfItem, newValue);
+
+// Array.prototype.splice
+vm.items.splice(indexOfItem, 1, newValue);
+
+// 인스턴스 메소드 vm.$set을 사용할 수 있음. ( 전역 Vue.set의 별칭임 )
+vm.$set(vm.items, indexOfItem, newValue);
+
+// 주의 사항 중 2번을 극복하기 위해 splice를 사용해야 함.
+vm.items.splice(newLength);
+
+/**
+ * 객체 변경 감지에 관한 주의사항
+ *
+ * JavaScript의 한계로 Vue는 속성 추가 및 삭제를 감지하지 못함.
+ */
+
+var vm = new Vue({
+  data: {
+    a: 1,
+  },
+});
+// 'vm.a'는 반응형
+
+vm.b = 2;
+// 'vm.b'는 반응형이 아님.
+
+/**
+ * Vue는 이미 만들어진 인스턴스에 새로운 루트 레벨의 반응형 속성을 동적으로
+ * 추가하는 것을 허용하지 않음.
+ * 그러나.
+ * Vue.set(object, propertyName, value) 메소드를 사용하여 중첩된 객체에
+ * 반응형 속성을 추가할 수 있음.
+ */
+
+var vm = new Vue({
+  data: {
+    userProfile: {
+      name: "Anika",
+    },
+  },
+});
+
+// 중첩된 userProfile 객체에 새로운 속성 age를 추가.
+vue.set(vm.userprofile, "age", 27);
+
+// 인스턴스 메소드인 vm.$set을 사용할 수도 잇음. Vue.set의 별칭임.
+vm.$set(vm.userProfile, "age", 27);
+
+// 때로는 Object.assign() 이나 _.extend()를 사용해 기존의 객체에 새 속성을
+// 할당할 수 잇음. 두 객체의 속성을 사용해 새 객체를 만들어야 함.
+Object.assign(vm.userProfile, {
+  age: 27,
+  favoriteColor: "Vue Green",
+});
+
+/**
+ * 필터링 / 정렬 된 결과 표시하기
+ *
+ * 때로 원본 데이터를 실제로 변경하거나 재설정하지 않고 배열의 필터링 된 버전이나
+ * 정렬된 버전을 표시해야 할 필요가 있습니다. 이 경우 필터링 된 배열이나
+ * 정렬된 배열을 반환하는 계산된 속성을 만들 수 있습니다.
+ */
+
+var vm = new Vue({
+  el: "#filtering",
+  data: {
+    numbers: [1, 2, 3, 4, 5],
+  },
+  computed: {
+    evenNumers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 0;
+      });
+    },
+  },
+});
+
+var vm = new Vue({
+  el: "#filtering2",
+  data: {
+    numbers: [1, 2, 3, 4, 5],
+  },
+  methods: {
+    even: function (numbers) {
+      return numbers.filter(function (number) {
+        return number % 2 === 0;
+      });
+    },
+  },
+});
