3ded22c0444e6fc100bf4d0b24f2f3bf2fc1d809
kr620666 <kr620666@amway.com>
2021-07-07 13:38:36 +0900

    컴포넌트

--- vue-guide/component.js
+++ vue-guide/component.js
@@ -384,3 +384,225 @@ var vm = new Vue({
     currentView: Home,
   },
 });
+
+var parent = new Vue({ el: "#parent" });
+// 자식 컴포넌트 인스턴스에 접근
+var child = parent.$refs.profile;
+
+/**
+ * ref가 v-for와 함께 사용될 때 얻을 수 있는 ref는 데이터 소스를 미러링하는
+ * 자식 컴포넌트를 포함하는 배열이 될 것.
+ *
+ * `$refs`는 컴포넌트가 렌더링 된 후에만 채워지며 반응적이지 않음.
+ * 직접 자식 조작을 위한 escape 해치를 의미
+ * 템플릿이나 계산 된 속성에서 `$refs`를 사용하지 말아야 함.
+ */
+
+/**
+ * # 비동기 컴포넌트
+ *
+ * 대규모 응용 프로그램에서는 응용프로그램을 더 작은 덩어리로 나누고 실제로 필요할 때만
+ * 서버에서 컴포넌트를 로드해야 할 수도 있음.
+ * Vue를 사용하면 컴포넌트 정의를 비동기식으로 해결하는 팩토리 함수로
+ * 컴포넌트를 정의할 수 있음.
+ * Vue는 컴포넌트가 실제로 렌더링 되어야 할 때만 팩토리 기능을 트리거하고
+ * 이후의 리렌더링을 위해 결과를 캐시함.
+ */
+
+Vue.component("async-example", function (resolve, reject) {
+  setTimeout(function () {
+    // 컴포넌트 정의를 resolve 콜백에 전달
+    resolve({
+      template: "<div>I am async!</div>",
+    });
+  }, 1000);
+});
+
+/**
+ * 팩토리 함수는 resolve 콜백을 받음.
+ * 이 콜백은 서버에서 컴포넌트 정의를 가져 왔을 때 호출 되어야 함.
+ * 또한, reject (reason)을 호출하여 로드가 실패 했음을 알릴 수 있음.
+ * 여기서 setTimeout은 데모용.
+ * 컴포넌트를 검색하는 방법은 전적으로 나에게 달려있음.
+ * 권장되는 접근법 중 하나는 webpack의 코드 분할기능과 함께
+ * 비동기 컴포넌트를 사용하는 것.
+ */
+
+Vue.component("async-webpack-example", function (resolve) {
+  // 이 특별한 require 구문은 Webpack이 Ajax 요청을 통해
+  // 로드되는 번들로 작성된 코드를 자동으로 분리하도록 지시함.
+  require(["./my-async-component"], resolve);
+});
+
+// factory 함수에서 Promise를 반환 할 수도 있음.
+Vue.component(
+  "async-webpack-example",
+  // 'import'함수는 'Promise'를 반환함
+  () => import("./my-async-component")
+);
+
+// 지역 등록을 사용하는 경우, Promise를 반환하는 함수를 제공할 수 있음.
+new Vue({
+  // ...
+  components: {
+    "my-component": () => import("./my-async-component"),
+  },
+});
+
+/**
+ * # 고급 비동기 컴포넌트
+ *
+ * 2.3 버전부터 비동기 컴포넌트 팩토리는 다음 형태의 객체를 반환할 수 있음.
+ */
+
+const AsyncComp = () => ({
+  // 로드하는 컴포넌트. 반드시 Promise 이어야 함.
+  component: import("./MyComp.vue"),
+  // 비동기 컴포넌트가 로드되는 동안 사용할 컴포넌트
+  loading: LoadingComp,
+  // 실패했을 경우 사용하는 컴포넌트
+  error: Errorcomp,
+  // 로딩 컴포넌트를 보여주기 전 지연하는 정도. 기본값 : 200mx
+  delay: 200,
+  // 시간이 초과되면 에러용 컴포넌트가 표시됨.
+  // 기본값: Infinity,
+  timeout: 3000,
+});
+
+/**
+ * vue-router에서 라우트 컴포넌트로 사용하는 경우
+ * 라우트 네비게이션이 발생하기 전에 비동기 컴포넌트가 먼저 작동하기 때문에
+ * 이러한 특성은 무시됨.
+ * 라우트 컴포넌트에서 위의 문법을 사용하려면 vue-router 2.4.0 이상을 사용해야 함.
+ */
+
+/**
+ * # 컴포넌트 이름 규약
+ *
+ * 컴포넌트 (또는 prop)를 등록 할 때 kebab-case, camelCase 또는 PascalCase를 사용할 수 있음.
+ */
+
+// 컴포넌트 정의에서
+new Vue({
+  components: {
+    // kebab-case를 사용한 등록
+    "kebab-cased-component": {
+      /**... */
+    },
+    // camelCase를 사용한 등록
+    camelCasedComponent: {
+      /** ... */
+    },
+    // PascalCase를 사용한 등록
+    PascalCasedComponent: {
+      /**...  */
+    },
+  },
+});
+
+/**
+ * 그러나 문자열 템플릿을 사용할 때 HTML의 대소문자를 구분하지 않음.
+ * 즉, 템플릿에서도 CamelCase, PascalCase 또는 kebab-case를 사용하여
+ * 컴포넌트와 prop을 참조할 수 있음.
+ *
+ * kebab-case
+ * camelCase를 사용하여 컴포넌트가 정의된 경우 camelCase 또는 kebab-case
+ * pascalCase를 사용하여 컴포넌트가 정의된 경우 kebab-case, camelCase or PascalCase
+ */
+
+new Vue({
+  components: {
+    "kebab-cased-component": {
+      /**.. */
+    },
+    camelCasedComponent: {
+      /** ..*/
+    },
+    PascalCasedComponent: {
+      /**.. */
+    },
+  },
+});
+
+/**
+ * # 재귀 컴포넌트
+ *
+ * 컴포넌트는 자신의 템플릿에서 재귀적으로 호출할 수 있음.
+ * name 옵션으로만 가능
+ * Vue.component를 사용하여 컴포넌트를 전역적으로 등록하면,
+ * 글로벌 ID가 컴포넌트의 name옵션으로 자동 설정됨.
+ *
+ * 주의하지 않으면 재귀적 컴포넌트로 인해 무한 루프가 발생할 수도 있음.
+ */
+
+Vue.component("unique-name-of-my-component", {
+  // ...
+  name: "stack-overflow",
+  template: "<div><stack-overflow></stack-overflow></div>",
+});
+
+// 위와 같은 컴포넌트는 '최대 스텍 크기 초과' 오류가 발생하므로
+// 재귀 호출이 조건부 (즉, 마지막에 false가 될 v-if를 사용)인지 확인.
+
+/**
+  * 컴포넌트가 실제로 렌더링 트리에서 서로의 자식 및 조상인 패러독스.
+    Vue.component를 이용해 전역으로 컴포넌트 등록 시 이 패러독스는 해결됨.
+    그러나, 모듈 시스템을 사용하여 컴포넌트를 필요로 하거나 가져오는 경우,
+    Webpack 또는 Browserify를 통해 오류가 발생.
+
+    '컴포넌트를 마운트하지 못했습니다 : 템플릿 또는 렌더링 함수가 정의되지 않았습니다.'
+
+    컴포넌트 A, B
+    모듈시스템은 우선 A가 필요함. 그러나 첫번째 A는 B를 필요로 하고,
+    B는 A를 필요로 함.
+    첫번째 A의 의존성을 해결하지 않고서는 무한 루프에 빠져버림.
+    이를 해결하려면 모듈 시스템에는 'A는 B를 필요로 하나 B를 먼저 해결할 필요가 없습니다.'
+    라고 말할 수 있는 지점을 제공해야 함.
+
+    여기서는 tree-folder 컴포넌트를 그 지점으로 함.
+    패러독스를 만드는 자식은 tree-folder-contents 컴포넌트.
+    beforeCreate 라이프 사이클 훅 시점까지 기다렸다가 해당 컴포넌트를 등록
+ */
+
+Vue.component({
+  beforeCreate: function () {
+    this.$options.components.TreeFolderContents = require("./tree-folder-contents.vue");
+  },
+});
+
+// 또 다른 방법으론, 컴포넌트를 지역등록 하는 경우에 Webpack의 비동기 import를 이용할 수 있음.
+
+new Vue({
+  components: {
+    TreeFoldercontents: () => import("./tree-folder-contents.vue"),
+  },
+});
+
+// 문제 해결!
+
+Vue.component("hello-world", {
+  template: "#hello-world-template",
+});
+
+/**
+ * 위 기능은 큰 템플릿이나 매우 작은 응용 프로그램의 데모에는 유용 할 수 있지만
+ * 템플릿을 나머지 컴포넌트 정의와 분리하기 떄문에 피해야 함.
+ */
+
+/**
+ * # v-once를 이용한 비용이 적게드는 정적 컴포넌트
+ *
+ * 일반 HTML 엘리먼트를 렌더링하는 것은 Vue에서 매우 빠르지만 가끔 정적 콘텐츠가 많이 포함 된
+ * 컴포넌트가 있을 수 있음. 이런 경우, v-once 디렉티브를 루트 엘리먼트에 추가함으로써
+ * 캐시가 한번만 실행되도록 할 수 있음.
+ */
+
+Vue.component("terms-of-service", {
+  template:
+    "\
+        <div v-once>\
+            <h1>Terms of Service</h1>\
+                ... a lot of static content ...\
+                    >/div>\
+                    ",
+});
