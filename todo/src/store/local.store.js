d8e2deefdbb7be7db19dae1255295fb32eb72d03
kr620666 <kr620666@amway.com>
2021-07-16 18:44:57 +0900

    local.store 추가

---
+++ todo/src/store/local.store.js
@@ -0,0 +1,71 @@
+class LocalStore {
+  constructor(id) {
+    this.key = "todo";
+  }
+
+  storeToJson() {
+    const store = localStorage.getItem(this.key);
+    let sources;
+    try {
+      sources = JSON.parse(store) || [];
+    } catch (e) {
+      sources = [];
+    }
+    return sources;
+  }
+
+  jsonToStore(stores) {
+    localStorage.setItem(this.key, JSON.stringify(stores));
+  }
+
+  save(data) {
+    const stores = this.storeToJson();
+    const map = {};
+    stores.forEach((store, idx) => (map[store.id] = idx));
+
+    if (!Array.isArray(data)) {
+      data = [data];
+    }
+
+    data.forEach((store) => {
+      // update
+      if (map[store.id] !== undefined) {
+        stores[map[store.id]] = store;
+      }
+      // append
+      else {
+        stores.push(store);
+      }
+    });
+    this.jsonToStore(stores);
+  }
+
+  forceSave(store) {
+    const stores = this.storeToJson();
+    stores.push(store);
+    this.jsonToStore(stores);
+  }
+
+  remove(id) {
+    const stores = this.storeToJson();
+    let index = null;
+    for (let i = 0; i < stores.length; i++) {
+      if (stores[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    stores.splice(index, 1);
+    this.jsonToStore(stores);
+  }
+
+  getAll() {
+    return this.storeToJson();
+  }
+
+  removeAll() {
+    this.jsonToStore([]);
+  }
+}
+
+export default LocalStore;
